8836f9b0d0e0e3b788f11a0a1790f6ff53c1fa61
Roguez84 <t.roguez984@gmail.com>
2021-04-20 14:31:20 -0400

    checked that there were appropriate tests

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -4,4 +4,13 @@ class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # If a book has a return date on the loans model,
+  # the status must be "en_estante"
+
+
+
+  # A book that has a loan date but not a return date,
+  # must have status "prestado"
+
 end
--- test/models/loan_test.rb
+++ test/models/loan_test.rb
@@ -4,4 +4,8 @@ class LoanTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+
+  # A book can have only one instance where the return date is blank
+
 end
